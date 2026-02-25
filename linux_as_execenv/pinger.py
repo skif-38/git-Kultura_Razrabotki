283200421dd709593394139d4e31c80292c1af7d
skif-38 <medvedpro24@gmail.com>
2026-02-25 16:16:06 +0800

    sdelali poddershku dati NOW

--- linux_as_execenv/pinger.py
+++ linux_as_execenv/pinger.py
@@ -1,6 +1,10 @@
 import requests
+from datetime import datetime
+
+
 url_template = "https://simurg.space/gen_file?data=obs&date={date}"
-date = "2026-02-19"
+now = datetime.now()
+date = now.strftime("%Y-%m-%d")
 url = url_template.format(date=date)
 response = requests.get(url = url, stream=True)
 print(f"for {date} got: ",response)
