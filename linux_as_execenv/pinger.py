fd53e53ed963c273b76682525e41d8ef6f5a751f
skif-38 <medvedpro24@gmail.com>
2026-02-25 16:08:29 +0800

    Added minimal working

---
+++ linux_as_execenv/pinger.py
@@ -0,0 +1,6 @@
+import requests
+url_template = "https://simurg.space/gen_file?data=obs&date={date}"
+date = "2026-02-19"
+url = url_template.format(date=date)
+response = requests.get(url = url, stream=True)
+print(f"for {date} got: ",response)
